f6535d7323f6b34a8b3a41aa0891387b06f0479f
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2022-08-29 22:50:54 +0200

    Added another table

--- bookstore_to_run.py
+++ bookstore_to_run.py
@@ -2,10 +2,11 @@ import sqlalchemy as db
 from sqlalchemy import create_engine
 from sqlalchemy.engine import URL
 from urllib.parse import unquote
-import pyodbc
+import pyodbc #do we need this?
 from sqlalchemy import text
 import pandas as pd
 import PySimpleGUI as sg
+from sqlalchemy.sql import func
 
 # CONNECTION
 
@@ -27,24 +28,33 @@ except Exception as e:
     print('Error while connecting to database:\n')
     print(e)
 
+metadata = db.MetaData()
+books = db.Table('Books', metadata, autoload=True, autoload_with=engine)
+stock = db.Table('Stock', metadata, autoload=True, autoload_with=engine)
+stores = db.Table('Stores', metadata, autoload=True, autoload_with=engine)
+authors2books = db.Table('Authors2Books', metadata, autoload=True, autoload_with=engine)
+authors = db.Table('Authors', metadata, autoload=True, autoload_with=engine)
 
 # GUI
 
-data = []
-headings = ['ISBN13', 'Title', 'Store', 'Number in Stock']
+book_data = []
+stock_data = []
+book_headings = ['ISBN13', 'Title', 'Firstname']
+stock_headings = ['Store', 'Number in Stock']
 
-layout1 = [[sg.Text("Welcome to Paige Turner's Bookstore")],
-           [sg.Text("Please enter book title.")],
-           [sg.InputText(key='Input')], 
-           [sg.Submit('Submit'), sg.Button('Exit', key='Exit1')]]
+layout1 = [[sg.Text("Welcome to Paige Turner's Bookstore!", font=('Avenir Next', 30))],
+           [sg.Text("Please enter the book title:", font=('Avenir', 15))],
+           [sg.InputText(key='-INPUT-', font=('Avenir', 15), size=50)], 
+           [sg.Submit('Submit', font=('Avenir', 12)), sg.Button('Exit', key='Exit1', font=('Avenir', 12))]]
 
-layout2 = [[sg.Text('Found the following results:')],
-           [sg.Table(key='-TABLE-', values=data, headings=headings)], 
-           [sg.Button('New Search'), sg.Button('Exit', key='Exit2')]]
-
+layout2 = [[sg.Text('Found the following results:', font=('Avenir', 15))],
+           [sg.Table(key='-TABLE1-', values=book_data, headings=book_headings, font=('Avenir', 12), enable_events=True)], 
+           [sg.Table(key='-TABLE2-', values=stock_data, headings=stock_headings, font=('Avenir', 12))], 
+           [sg.Button('New Search', font=('Avenir', 12)), sg.Button('Exit', key='Exit2', font=('Avenir', 12))]]
+#auto_size_columns=False col_width
 layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-')]]
 
-window = sg.Window('Bookstore Search', layout, margins=(300, 150))
+window = sg.Window('Bookstore Search', layout, keep_on_top=True, element_justification='c')
 
 layout = 1  # The currently visible layout
 
@@ -52,107 +62,60 @@ while True:
     event, values = window.read()
     print(event, values)
 
-    
-
     if event in (None, 'Exit1', 'Exit2'):
         break
 
-    if event == 'Submit':
+    if event == '-TABLE1-':
 
-        window['-TABLE-'].update('')
+        stock_data = []
+
+        selected_row = values['-TABLE1-'][0]
+        isbn13 = book_data[selected_row][0]
+        
+        with engine.connect() as conn: 
+            
+            stock_data = []
+
+            query = db.select([stores.columns.Name, stock.columns.NumberInStock]).filter(books.columns.ISBN13 == isbn13).join(stock, books.columns.ISBN13 == stock.columns.ISBN13).join(stores, stock.columns.StoreID == stores.columns.ID)
+
+            results = conn.execute(query).fetchall()
+
+            for result in results:
+                stock_data.append(list(result))
+
+        window['-TABLE2-'].update(values=stock_data)
+
+    if event == 'Submit':
+        book_data = []
+
         # Query data
         with engine.connect() as conn: 
-            metadata = db.MetaData()
-            books = db.Table('Books', metadata, autoload=True, autoload_with=engine)
-            stock = db.Table('Stock', metadata, autoload=True, autoload_with=engine)
-            stores = db.Table('Stores', metadata, autoload=True, autoload_with=engine)
 
-            search = "%{}%".format(values['Input'])
+            search = "%{}%".format(values['-INPUT-'])
 
-            query = db.select([books.columns.ISBN13, 
-                            books.columns.Title, 
-                            stores.columns.Name, 
-                            stock.columns.NumberInStock]).filter(books.columns.Title.like(search)).join(stock, books.columns.ISBN13 == stock.columns.ISBN13).join(stores, stock.columns.StoreID == stores.columns.ID)
+            query = db.select([books.columns.ISBN13, books.columns.Title, func.concat(authors.columns.FirstName, ' ', authors.columns.Initials, ' ', authors.columns.LastName)]).filter(books.columns.Title.like(search)).join(authors2books, books.columns.ISBN13 == authors2books.columns.ISBN13).join(authors, authors2books.columns.AuthorID == authors.columns.ID)
 
             results = conn.execute(query).fetchall()
         
-        # Change format
+        # Change tuple to list
         for result in results:
-            data.append(list(result))
-        
+            book_data.append(list(result))
+
         # Update layout
-        window['-TABLE-'].update(values=data)
+        window['-TABLE1-'].update(values=book_data)
+
+
+
+
+
         window[f'-COL{layout}-'].update(visible=False)
         layout = layout + 1 
         window[f'-COL{layout}-'].update(visible=True)
-    elif event == 'New Search': #update search field with empty string
-        window['Input'].update('')
+
+    elif event == 'New Search': 
+        window['-INPUT-'].update('')
         window[f'-COL{layout}-'].update(visible=False)
         layout = layout -1 
         window[f'-COL{layout}-'].update(visible=True)
         
-
 window.close()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-layout1 = [[sg.Text("Welcome to Paige Turner's Bookstore")], 
-         [sg.Text("Please enter book title.")], 
-         [sg.InputText()], 
-         [sg.Submit('submit_button'), sg.Cancel()]]
-
-layout2 = [[sg.Text()]]
-
-window = sg.Window(title="Bookstore", layout=[[layout1]], margins=(300, 150))
-'''
-
-'''
-# Create an event loop
-while True:
-    event, values = window.read()
-
-    #inputed_title = input("Enter book title.")
-
-    with engine.connect() as conn: 
-        
-        metadata = db.MetaData()
-        books = db.Table('Books', metadata, autoload=True, autoload_with=engine)
-        stock = db.Table('Stock', metadata, autoload=True, autoload_with=engine)
-        stores = db.Table('Stores', metadata, autoload=True, autoload_with=engine)
-
-        search = "%{}%".format(values[0])
-
-        query = db.select([books.columns.ISBN13, 
-                        books.columns.Title, 
-                        stores.columns.Name, 
-                        stock.columns.NumberInStock]).filter(books.columns.Title.like(search)).join(stock, books.columns.ISBN13 == stock.columns.ISBN13).join(stores, stock.columns.StoreID == stores.columns.ID)
-
-        ResultProxy = conn.execute(query)
-        ResultSet = pd.DataFrame(ResultProxy.fetchall())
-    
-    #if event == 'submit_button':
-
-
-    print(ResultSet)
-        # End program if user closes window or
-        # presses the OK button
-    if event == "OK" or event == sg.WIN_CLOSED:
-        break
-
-window.close()
-
-'''
-
-
