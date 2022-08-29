e8750fed3cedaa665a00ba3485547c2e00916985
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2022-08-30 01:08:53 +0200

    almost done

--- bookstore_to_run.py
+++ bookstore_to_run.py
@@ -47,48 +47,54 @@ layout1 = [[sg.Text("Welcome to Paige Turner's Bookstore!", font=('Avenir Next',
            [sg.InputText(key='-INPUT-', font=('Avenir', 15), size=50)], 
            [sg.Submit('Submit', font=('Avenir', 12)), sg.Button('Exit', key='Exit1', font=('Avenir', 12))]]
 
-layout2 = [[sg.Text('Found the following results:', font=('Avenir', 15))],
+layout2 = [[sg.Text('Found the following results (click on item for stock info):', font=('Avenir', 15))],
            [sg.Table(key='-TABLE1-', values=book_data, headings=book_headings, font=('Avenir', 12), justification='c', col_widths = [15, 30, 15], auto_size_columns=False,  enable_events=True)], 
            [sg.Table(key='-TABLE2-', values=stock_data, headings=stock_headings, col_widths = [30, 30], auto_size_columns=False, font=('Avenir', 12), justification='c', num_rows=3, hide_vertical_scroll=True)], 
-           [sg.Button('New Search', font=('Avenir', 12)), sg.Button('Exit', key='Exit2', font=('Avenir', 12))]]
+           [sg.Button('New Search', key='Search1', font=('Avenir', 12)), sg.Button('Exit', key='Exit2', font=('Avenir', 12))]]
 
 layout3 = [[sg.Text('Your search did not match any titles. Please try again or exit.', font=('Avenir', 15))],
-           [sg.Button('New Search', font=('Avenir', 12)), sg.Button('Exit', key='Exit2', font=('Avenir', 12))]]
+           [sg.Button('New Search', key='Search2', font=('Avenir', 12)), sg.Button('Exit', key='Exit3', font=('Avenir', 12))]]
 
-layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout2, visible=False, key='-COL3-')]]
+layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')]]
 
 window = sg.Window('Bookstore Search', layout, keep_on_top=True, element_justification='c')
 
 layout = 1  # The currently visible layout
 
+count = 0
 while True:
     event, values = window.read()
     print(event, values)
 
-    if event in (None, 'Exit1', 'Exit2'):
+    if event in (None, 'Exit1', 'Exit2', 'Exit3'):
         break
 
     if event == '-TABLE1-':
 
         stock_data = []
 
-        selected_row = values['-TABLE1-'][0]
-        isbn13 = book_data[selected_row][0]
-        
-        with engine.connect() as conn: 
+        if len(values['-TABLE1-']) != 0:
+            selected_row = values['-TABLE1-'][0]
+            isbn13 = book_data[selected_row][0]
+
+            with engine.connect() as conn: 
+                
+                stock_data = []
+
+                query = db.select([stores.columns.Name, stock.columns.NumberInStock]).filter(books.columns.ISBN13 == isbn13).join(stock, books.columns.ISBN13 == stock.columns.ISBN13).join(stores, stock.columns.StoreID == stores.columns.ID)
+
+                results = conn.execute(query).fetchall()
+
+                for result in results:
+                    stock_data.append(list(result))
+
+            window['-TABLE2-'].update(values=stock_data)
+
+        else:
+            window['-TABLE2-'].update(values=['', ''])
             
-            stock_data = []
-
-            query = db.select([stores.columns.Name, stock.columns.NumberInStock]).filter(books.columns.ISBN13 == isbn13).join(stock, books.columns.ISBN13 == stock.columns.ISBN13).join(stores, stock.columns.StoreID == stores.columns.ID)
-
-            results = conn.execute(query).fetchall()
-
-            for result in results:
-                stock_data.append(list(result))
-
-        window['-TABLE2-'].update(values=stock_data)
-
     if event == 'Submit':
+
         book_data = []
 
         # Query data
@@ -108,17 +114,17 @@ while True:
         if len(book_data) == 0:
             window[f'-COL{layout}-'].update(visible=False)
             layout = layout + 2 
-            window[f'-COL{layout}-'].update(visible=True)            
-        else: 
+            window[f'-COL{layout}-'].update(visible=True)           
+        else:
             window['-TABLE1-'].update(values=book_data)
             window[f'-COL{layout}-'].update(visible=False)
             layout = layout + 1 
             window[f'-COL{layout}-'].update(visible=True)
 
-    elif event == 'New Search': 
+    elif event in ('Search1', 'Search2'): 
         window['-INPUT-'].update('')
         window[f'-COL{layout}-'].update(visible=False)
-        layout = layout -1 
-        window[f'-COL{layout}-'].update(visible=True)
+        layout = 1
+        window[f'-COL1-'].update(visible=True)
         
 window.close()
